f49546a7c9d02cc9d52fd3981c35ac4958991564
lody <lody@MacBook-Pro-de-lody.local>
2023-02-21 03:02:02 -0800

    mise en place du cv colonne gauche

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -26,38 +26,78 @@ function Home() {
             <h1>Experiences</h1>
           </div>
           <div className='resume'>
-            <div className='formation'>
-              <div className='title-formation'>
-                <h2>Formation</h2>
+            <div className='resume-left'>
+              <div className='formation'>
+                <div className='title-formation'>
+                  <h2>Formation</h2>
+                </div>
+                <div className='block-formation'>
+                    <div className='date'>
+                      <p className='subtitle-resume'>2021-2022</p>
+                      <p className='ligne'></p>
+                    </div>
+                    <div className='aboutformation'>
+                      <p className='subtitle-resume'>Front-end (titre RNCP niveau 5)Formation Web Développeuse</p>
+                      <p className='where-formation'>Openclassrooms</p>
+                      <ul>
+                        <li>-Optimisez un site web existant (pratiques de SEO)</li>
+                        <li>-Créez le backend et l'API d'une application</li>
+                        <li>-Construisez un site e-commerce (A l’aide du JavaScript )</li>
+                        <li>-Créez un réseau social d’entreprise (bases de données SQL, React.js)</li>
+                      </ul>
+                    </div>
+                </div>
+                <div className='block-formation'>
+                    <div className='date'>
+                      <p className='subtitle-resume'>2017-2019</p>
+                      <p className='ligne'></p>
+                    </div>
+                    <div className='aboutformation'>
+                      <p className='subtitle-resume'>Bachelor Concepteur Designer Graphique</p>
+                      <p className='where-formation'>Axe Sud Toulouse (école condé)</p>
+                      <p>Design Graphique et Communication Visuelle</p>
+                    </div>
+                </div>
               </div>
-              <div className='block-formation'>
-                  <div className='date'>
-                    <p className='subtitle-resume'>2021-2022</p>
-                    <p className='ligne'></p>
-                  </div>
-                  <div className='aboutformation'>
-                    <p className='subtitle-resume'>Front-end (titre RNCP niveau 5)Formation Web Développeuse</p>
-                    <p className='where-formation'>Openclassrooms</p>
-                    <ul>
-                      <li>-Optimisez un site web existant (pratiques de SEO)</li>
-                      <li>-Créez le backend et l'API d'une application</li>
-                      <li>-Construisez un site e-commerce (A l’aide du JavaScript )</li>
-                      <li>-Créez un réseau social d’entreprise (bases de données SQL, React.js)</li>
-                    </ul>
-                  </div>
+              <div className='formation'>
+                <div className='title-formation'>
+                  <h2>Expériences Professionnelles</h2>
+                </div>
+                <div className='block-formation'>
+                    <div className='date'>
+                      <p className='subtitle-resume'>Aout-2021/2022</p>
+                      <p className='ligne'></p>
+                    </div>
+                    <div className='aboutformation'>
+                      <p className='subtitle-resume'>Développeuse Front-end <span>(1 an)</span></p>
+                      <p className='where-formation'>50A agence digitale</p>
+                      <p>Développeuse front-end /Intégratrice /Webmaster</p>
+                    </div>
+                </div>
+                <div className='block-formation'>
+                    <div className='date'>
+                      <p className='subtitle-resume'>Juin-Nov 2020</p>
+                      <p className='ligne'></p>
+                    </div>
+                    <div className='aboutformation'>
+                      <p className='subtitle-resume'>Conseillère en vente <span>(6 mois)</span></p>
+                      <p className='where-formation'>Rougier & plé</p>
+                      <p>Conseillère en vente d’articles d’art</p>
+                    </div>
+                </div>
+                <div className='block-formation'>
+                    <div className='date'>
+                      <p className='subtitle-resume'>Fév- Mars 2020</p>
+                      <p className='ligne'></p>
+                    </div>
+                    <div className='aboutformation'>
+                      <p className='subtitle-resume'>Graphiste<span>(freelance 1 mois)</span></p>
+                      <p className='where-formation'>Gossiperz/influence4u</p>
+                      <p>exécution cartes de visite, bannière web, Identité d’agence d’architecture</p>
+                    </div>
+                </div>
               </div>
-              <div className='block-formation'>
-                  <div className='date'>
-                    <p className='subtitle-resume'>2017-2019</p>
-                    <p className='ligne'></p>
-                  </div>
-                  <div className='aboutformation'>
-                    <p className='subtitle-resume'>Bachelor Concepteur Designer Graphique</p>
-                    <p className='where-formation'>Axe Sud Toulouse (école condé)</p>
-                    <p>Design Graphique et Communication Visuelle</p>
-                  </div>
-              </div>
-            </div>
+            </div> 
           </div>
         </div>
    </div>
